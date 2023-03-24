9844f81b0f6059b59ffe1c92bddf0036fc7e008f
Pascal Horton <pascal.horton@giub.unibe.ch>
2023-03-24 16:12:46 +0100

    Adding the directories as arguments.

--- src/extract_claims.py
+++ src/extract_claims.py
@@ -1,7 +1,9 @@
 from core import targets
+from utils.config import Config
 
+config = Config()
 
 targets = targets.Targets()
-targets.load_contracts()
-targets.load_damages()
+targets.load_contracts(config.get('DIR_CONTRACTS'))
+targets.load_damages(config.get('DIR_DAMAGES'))
 
