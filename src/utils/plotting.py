6707ba42fff76b4336e4bddbb5f2fcc69f39df4c
Pascal Horton <pascal.horton@giub.unibe.ch>
2023-05-03 14:29:40 +0200

    Adding histogram plotting function.

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,8 +1,10 @@
+import re
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 
-def plot_heatmap_differences(data, total, labels, title):
+def plot_heatmap_differences(data, total, labels, title, dir_output=None):
     fig, ax = plt.subplots(figsize=(6, 6))
     im = ax.imshow(data)
 
@@ -25,4 +27,29 @@ def plot_heatmap_differences(data, total, labels, title):
 
     ax.set_title(title)
     fig.tight_layout()
-    plt.show()
+
+    _save_or_show(dir_output, title)
+
+
+def plot_histogram_time_difference(claims, field_name, title, dir_output=None):
+    hist_bins = claims[field_name].max() - claims[field_name].min() + 1
+    hist_range = (claims[field_name].min() - 0.5, claims[field_name].max() + 0.5)
+    counts, bins = np.histogram(claims[field_name], bins=hist_bins, range=hist_range)
+    plt.stairs(counts, bins)
+    plt.title(title)
+    plt.xlabel("Difference in days")
+    plt.ylabel("Count")
+    plt.tight_layout()
+
+    _save_or_show(dir_output, title)
+
+
+def _save_or_show(dir_output, title):
+    if dir_output is not None:
+        dir_output = Path(dir_output)
+        filename = re.sub(r'\W+', '', title.replace(' ', '_'))
+        plt.savefig(dir_output / (filename + '.png'), dpi=600)
+        plt.savefig(dir_output / (filename + '.pdf'), dpi=600)
+        plt.close()
+    else:
+        plt.show()
