04967d092c1c6d950befaed61303b5c7483bdb77
Pascal Horton <pascal.horton@giub.unibe.ch>
2023-05-10 17:08:36 +0200

    Updating the damage/event linking analysis.

--- src/analyze_claims_events_matching.py
+++ src/analyze_claims_events_matching.py
@@ -1,8 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from datetime import datetime, time, timedelta
-
 import core.damages
 import core.events
 import utils.plotting
@@ -12,38 +8,59 @@ from pathlib import Path
 config = Config()
 
 criteria_list = [
-    ['i_mean'],
-    ['i_max'],
-    ['p_sum'],
-    ['i_mean', 'i_max', 'p_sum'],
-    ['i_mean', 'i_max', 'p_sum', 'r_ts_win'],
-    ['i_mean', 'i_max', 'p_sum', 'r_ts_evt'],
-    ['i_mean', 'i_max', 'p_sum', 'r_ts_win', 'r_ts_evt'],
+    ['i_max', 'p_sum'],
+    ['i_max', 'p_sum'],
+    ['i_max', 'p_sum', 'r_ts_win'],
+    ['i_max', 'p_sum', 'r_ts_win'],
+    ['i_max', 'p_sum', 'r_ts_evt'],
+    ['i_max', 'p_sum', 'r_ts_evt'],
+    ['i_max', 'p_sum', 'r_ts_win', 'r_ts_evt'],
     ['i_max', 'p_sum', 'r_ts_win', 'r_ts_evt'],
     ['prior', 'i_max', 'p_sum'],
+    ['prior', 'i_max', 'p_sum'],
+    ['prior', 'i_max', 'p_sum', 'r_ts_win', 'r_ts_evt'],
     ['prior', 'i_max', 'p_sum', 'r_ts_win', 'r_ts_evt'],
 ]
 
 labels = [
-    'i_mean',
-    'i_max',
-    'p_sum',
-    '3 qty',
-    '3 qty + r_ts_win',
-    '3 qty + r_ts_evt',
-    'original',
+    'v1',
+    'v1 even',
     'v2',
+    'v2 even',
     'v3',
+    'v3 even',
+    'v4',
+    'v4 even',
+    'v5',
+    'v5 even',
+    'v6',
+    'v6 even',
 ]
 
-window_days = [5, 3, 1]
-#window_days = [9, 7, 5, 3, 1]
+window_days = [
+    [5, 3, 1],
+    [6, 4, 2],
+    [5, 3, 1],
+    [6, 4, 2],
+    [5, 3, 1],
+    [6, 4, 2],
+    [5, 3, 1],
+    [6, 4, 2],
+    [5, 3, 1],
+    [6, 4, 2],
+]
+
+damage_categories = ['external']
 
 tmp_dir = config.get('TMP_DIR')
 
+plot_histograms = True
+plot_matrix = True
+
 # Compute the different matching
 for i, criteria in enumerate(criteria_list):
-    filename = f'damages_matched_conf_{i}.pickle'
+    label = labels[i].replace(" ", "_")
+    filename = f'damages_linked_{label}.pickle'
     file_path = Path(tmp_dir + '/' + filename)
 
     if file_path.exists():
@@ -51,19 +68,19 @@ for i, criteria in enumerate(criteria_list):
         continue
 
     print(f"Assessing criteria {criteria}")
-    damages = core.damages.Damages(cid_file=config.get('CID_PATH'),
-                                   dir_contracts=config.get('DIR_CONTRACTS'),
+    damages = core.damages.Damages(dir_contracts=config.get('DIR_CONTRACTS'),
                                    dir_claims=config.get('DIR_CLAIMS'))
-    damages.select_all_categories()
+    damages.select_categories_type(damage_categories)
 
     events = core.events.Events()
     events.load_events_and_select_locations(config.get('EVENTS_PATH'), damages)
 
-    damages.match_with_events(events, criteria=criteria, filename=filename,
-                              window_days=window_days)
+    damages.link_with_events(events, criteria=criteria, filename=filename,
+                             window_days=window_days[i])
 
 # Load the first pickle file and do some common work
-damages = core.damages.Damages(pickle_file=f'damages_matched_conf_{0}.pickle')
+damages = core.damages.Damages(
+    pickle_file=f'damages_linked_{labels[0].replace(" ", "_")}.pickle')
 total = damages.claims.eid.astype(bool).sum()
 events = core.events.Events()
 events.load_events_and_select_locations(config.get('EVENTS_PATH'), damages)
@@ -72,7 +89,8 @@ del damages
 # Compare the events assigned
 diff_count = np.zeros((len(criteria_list), len(criteria_list)))
 for i_ref, criteria_ref in enumerate(criteria_list):
-    filename_ref = f'damages_matched_conf_{i_ref}.pickle'
+    label_ref = labels[i_ref].replace(" ", "_")
+    filename_ref = f'damages_linked_{label_ref}.pickle'
     df_ref = core.damages.Damages(pickle_file=filename_ref)
     assert total == df_ref.claims.eid.astype(bool).sum()
 
@@ -81,25 +99,28 @@ for i_ref, criteria_ref in enumerate(criteria_list):
     df_ref.compute_days_to_event_start('dt_start')
     df_ref.compute_days_to_event_center('dt_center')
 
-    utils.plotting.plot_histogram_time_difference(
-        df_ref.claims, field_name='dt_start', dir_output=config.get('OUTPUT_DIR'),
-        title=f"Difference (in days) between the claim date and the event start \n"
-        f"when using '{labels[i_ref]}'")
+    if plot_histograms:
+        utils.plotting.plot_histogram_time_difference(
+            df_ref.claims, field_name='dt_start', dir_output=config.get('OUTPUT_DIR'),
+            title=f"Difference (in days) between the claim date and the event start \n"
+            f"when using '{labels[i_ref]}'")
 
-    utils.plotting.plot_histogram_time_difference(
-        df_ref.claims, field_name='dt_center', dir_output=config.get('OUTPUT_DIR'),
-        title=f"Difference (in days) between the claim date and the event center \n"
-        f"when using '{labels[i_ref]}'")
+        utils.plotting.plot_histogram_time_difference(
+            df_ref.claims, field_name='dt_center', dir_output=config.get('OUTPUT_DIR'),
+            title=f"Difference (in days) between the claim date and the event center \n"
+            f"when using '{labels[i_ref]}'")
 
     # Compute the differences in events attribution with other criteria
     for i_diff, criteria_diff in enumerate(criteria_list):
-        filename_diff = f'damages_matched_conf_{i_diff}.pickle'
+        label_diff = labels[i_diff].replace(" ", "_")
+        filename_diff = f'damages_linked_{label_diff}.pickle'
         df_comp = core.damages.Damages(pickle_file=filename_diff)
         assert len(df_ref.claims) == len(df_comp.claims)
 
         diffs = df_ref.claims.eid - df_comp.claims.eid
         diff_count[i_ref, i_diff] = diffs.astype(bool).sum()
 
-utils.plotting.plot_heatmap_differences(
-    diff_count, total, labels, dir_output=config.get('OUTPUT_DIR'),
-    title="Differences in the event-damage attribution")
+if plot_matrix:
+    utils.plotting.plot_heatmap_differences(
+        diff_count, total, labels, dir_output=config.get('OUTPUT_DIR'),
+        title="Differences in the event-damage attribution")
--- src/extract_claims.py
+++ src/extract_claims.py
@@ -12,4 +12,4 @@ damages.select_all_categories()
 events = core.events.Events()
 events.load_events_and_select_locations(config.get('EVENTS_PATH'), damages)
 
-damages.match_with_events(events)
+damages.link_with_events(events)
