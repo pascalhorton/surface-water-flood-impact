ffb8e9b3434513a38a2002b9832d479e00ac86c2
Pascal Horton <pascal.horton@giub.unibe.ch>
2023-05-31 17:48:33 +0200

    Updating forecast verification.

--- src/utils/forecast_verification.py
+++ src/utils/forecast_verification.py
@@ -27,7 +27,7 @@ def compute_confusion_matrix(df, field_obs='target', field_model='predict'):
     return tp, tn, fp, fn
 
 
-def binary_detection(metric, tp, fp, fn, tn):
+def compute_score(metric, tp, tn, fp, fn):
     """
     Metrics based on the confusion matrix.
 
@@ -37,12 +37,12 @@ def binary_detection(metric, tp, fp, fn, tn):
         The desired metric
     tp: int
         Cases where obs is true and model predicts occurrence
+    tn: int
+        Cases where obs is false and model does not predict occurrence
     fp: int
         Cases where obs is false and model predicts occurrence
     fn: int
         Cases where obs is true and model does not predict occurrence
-    tn: int
-        Cases where obs is false and model does not predict occurrence
 
     Returns
     -------
@@ -60,28 +60,28 @@ def binary_detection(metric, tp, fp, fn, tn):
 
     if metric in ['base_rate', 'brate']:
         # Base rate (brate) = (a+c) / n
-        # [0,1]
+        # [0, 1]
         brate = (tp + fn) / n
         assert (0 <= brate <= 1)
         return brate
 
     elif metric in ['forecast_rate', 'frate']:
         # Forcast rate (frate) = (a+b) / n
-        # [0,1]
+        # [0, 1]
         frate = (tp + fp) / n
         assert (0 <= frate <= 1)
         return frate
 
     elif metric in ['bias']:
         # Frequency Bias (bias) = (a+b) / (a+c)
-        # [0,inf]
+        # [0, inf]
         bias = (tp + fp) / (tp + fn)
         assert bias >= 0
         return bias
 
     elif metric in ['hit_rate', 'H']:
         # Hit rate (H) = a / (a+c)
-        # [0,1]
+        # [0, 1]
         h = tp / (tp + fn)
         assert (0 <= h <= 1)
         return h
@@ -89,28 +89,28 @@ def binary_detection(metric, tp, fp, fn, tn):
     elif metric in ['false_alarm_rate', 'F']:
         # False alarm rate (F) = b / (b+d)
         # Note f != false alarm ratio = b / (a+b)
-        # [0,1]
+        # [0, 1]
         f = fp / (fp + tn)
         assert (0 <= f <= 1)
         return f
     
     elif metric in ['false_alarm_ratio', 'FAR']:
         # False alarm ratio (FAR) = b / (a+b)
-        # [0,1]
+        # [0, 1]
         far = fp / (tp + fp)
         assert (0 <= far <= 1)
         return far
     
     elif metric in ['proportion_correct', 'PC']:
         # Proportion Correct (PC) = (a+d) / n
-        # [0,1]
+        # [0, 1]
         pc = (tp + tn) / n
         assert (0 <= pc <= 1)
         return pc
 
     elif metric in ['success_index', 'SI']:
         # Success index (si) = 1/2 (a/(a+c) + d/(b+d))
-        # [0,1]
+        # [0, 1]
         si = 1 / 2 * (tp / (tp + fn) +
                       tn / (fp + tn))
         assert (0 <= si <= 1)
@@ -118,14 +118,14 @@ def binary_detection(metric, tp, fp, fn, tn):
 
     elif metric in ['critical_success_index', 'CSI']:
         # Critical Sucess Index (CSI) = Thread score = a / (a+b+c)
-        # [0,1]
-        csi = (tp) / (tp + fp + fn)
+        # [0, 1]
+        csi = tp / (tp + fp + fn)
         assert (0 <= csi <= 1)
         return csi
 
     elif metric in ['gilbert_skill_score', 'GSS']:
         # Gilbert Skill Score (GSS) = (a-a_exp) / (a+b+c-a_exp)
-        # [-1/3,1]
+        # [-1/3, 1]
         # Expected a_obs1_mod1 for random forecast a_ex) = (a+b) * (a+c) / n
         a_exp = (tp + fp) * (tp + fn) / n
         gss = (tp - a_exp) / (tp + fp + fn - a_exp)
@@ -134,7 +134,7 @@ def binary_detection(metric, tp, fp, fn, tn):
 
     elif metric in ['heidke_skill_score', 'HSS']:
         # Heidke Skill Score (HSS) = (a+d-a_exp-d_exp) / (n-a_exp-d_exp)
-        # [-1,1]
+        # [-1, 1]
         # Expected d_obs0_mod0 for random forecast a_exp = (b+d) * (c+d) / n
         a_exp = (tp + fp) * (tp + fn) / n
         d_exp = (fp + tn) * (fn + tn) / n
@@ -144,7 +144,7 @@ def binary_detection(metric, tp, fp, fn, tn):
 
     elif metric in ['peirce_skill_score', 'PSS']:
         # Peirce Skill Score (PSS) = (ad-bc) / ((b+d)(a+c)) = H - F
-        # [-1,1]
+        # [-1, 1]
         h = tp / (tp + fn)
         f = fp / (fp + tn)
         pss = h - f
@@ -153,7 +153,7 @@ def binary_detection(metric, tp, fp, fn, tn):
 
     elif metric in ['clayton_skill_score', 'CSS']:
         # Clayton Skill Score (css) = a/(a+b) - c/(c+d)
-        # [-1,1]
+        # [-1, 1]
         css = tp / (tp + fp) - fn / (
                 fn + tn)
         assert (-1 <= css <= 1)
@@ -169,14 +169,14 @@ def binary_detection(metric, tp, fp, fn, tn):
 
     elif metric in ['odds_ratio', 'OR']:
         # Odds Ratio (OR) = ad / bc
-        # [0,Inf]
+        # [0, Inf]
         odr = tp * tn / (fp * fn)
         assert (odr >= 0)
         return odr
 
     elif metric in ['log_odds_ratio', 'LOR']:
         # Log of Odds Ratio (LOR) = ln(ad / bc)
-        # [-Inf,Inf]
+        # [-Inf, Inf]
         odr = tp * tn / (fp * fn)
         lodr = math.log(odr)
         return lodr
@@ -184,7 +184,7 @@ def binary_detection(metric, tp, fp, fn, tn):
     elif metric in ['odds_ratio_skill_score', 'ORSS']:
         # Odds Ratio Skill Score (ORSS) = Yule's Q = (ad-bc) / (ad+bc) =
         # (or - 1) / (or + 1) = (H - F) / (H(1-F) + F(1-H))
-        # [-1,1]
+        # [-1, 1]
         odr = tp * tn / (fp * fn)
         if math.isinf(odr):  # orss has no skill when odr = Inf
             return 0
@@ -227,8 +227,9 @@ def binary_detection(metric, tp, fp, fn, tn):
         return edi
 
     elif metric in ['symmetric_extremal_dependency_index', 'SEDI']:
-        # Symmetric Extremal Dependence Index (SEDI) = (ln(F)-ln(H)+ln(1-H)-ln(1-F)) /
-        #                                              (ln(F)+ln(H)+ln(1-H)+ln(1-F))
+        # Symmetric Extremal Dependence Index
+        # (SEDI) = (ln(F)-ln(H)+ln(1-H)-ln(1-F)) /
+        #          (ln(F)+ln(H)+ln(1-H)+ln(1-F))
         h = tp / (tp + fn)
         f = fp / (fp + tn)
 
