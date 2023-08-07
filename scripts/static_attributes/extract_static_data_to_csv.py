9427d0e4093580e0bc05813f64113a20d54df6f0
Pascal Horton <pascal.horton@giub.unibe.ch>
2023-08-07 21:00:41 +0200

    Fixing data extraction script.

--- scripts/static_attributes/extract_static_data_to_csv.py
+++ scripts/static_attributes/extract_static_data_to_csv.py
@@ -21,7 +21,6 @@ data_dir = Path(base_dir + '/' + SRC_DIR)
 
 def main():
     domain = Domain(config.get('CID_PATH'))
-    cid_file = config.get('CID_PATH')
 
     # List all .tif files in the source directory
     tif_files = [f for f in data_dir.glob('*.tif')]
@@ -34,7 +33,7 @@ def main():
         with rasterio.open(f) as dataset:
             domain.check_projection(dataset, f)
 
-        df_new = extract_statistics(cid_file, f)
+        df_new = extract_statistics(domain, f)
 
         # Merge the statistics to the DataFrame by the column cid
         if df.empty:
@@ -48,12 +47,9 @@ def main():
     print('Done.')
 
 
-def extract_statistics(cids_path, data_path):
+def extract_statistics(domain, data_path):
     # Open the cids.tif and data files using rasterio
-    with rasterio.open(cids_path) as cids_ds, rasterio.open(data_path) as data_ds:
-        # Read the raster data as numpy arrays
-        cids_data = cids_ds.read(1)
-
+    with rasterio.open(data_path) as data_ds:
         # Initialize empty lists to store statistics for each cell in cids_data
         cids_vals = []
         min_vals = []
@@ -63,32 +59,34 @@ def extract_statistics(cids_path, data_path):
         median_vals = []
 
         # Iterate over each cell in cids_data
-        for i in range(cids_data.shape[0]):
-            for j in range(cids_data.shape[1]):
-                if math.isnan(cids_data[i, j]):
+        cids_map = domain.cids['ids_map']
+        for i in range(cids_map.shape[0]):
+            for j in range(cids_map.shape[1]):
+                cid = cids_map[i, j]
+                if math.isnan(cid) or cid == 0:
                     continue
 
                 # Calculate the bounding box of the corresponding cell in the data
-                x, y = cids_ds.xy(j, i)
-                res = cids_ds.res[0]
+                x = domain.cids['xs'][i, j]
+                y = domain.cids['ys'][i, j]
+                res = domain.resolution[0]
                 bbox = rasterio.coords.BoundingBox(
                     x - res / 2, y - res / 2, x + res / 2, y + res / 2
                 )
 
                 # Read the data within the bounding box
-                window = data_ds.window(*bbox)
                 data_cells = data_ds.read(1, window=data_ds.window(*bbox))
+                data_cells[data_cells == data_ds.nodata] = np.nan
+                data_cells[data_cells == -9999] = np.nan
 
                 # Skip empty cells
                 if data_cells.shape[0] == 0 or data_cells.shape[1] == 0:
                     continue
                 if np.isnan(data_cells).all():
                     continue
-                if np.any(data_cells == -9999):
-                    continue
 
                 # Calculate statistics and append them to the lists
-                cids_vals.append(int(cids_data[i, j]))
+                cids_vals.append(int(cid))
                 min_vals.append(np.nanmin(data_cells))
                 max_vals.append(np.nanmax(data_cells))
                 mean_vals.append(np.nanmean(data_cells))
