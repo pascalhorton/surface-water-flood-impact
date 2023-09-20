759ce232a435cb913ade931b5bdc754959f92eb8
Pascal Horton <pascal.horton@unibe.ch>
2023-09-20 15:21:39 +0200

    Adding benchmark model assessments.

---
+++ scripts/impact_functions/benchmark_damage_ratio.py
@@ -0,0 +1,41 @@
+"""
+Train a random forest model to predict the occurrence of damages.
+"""
+
+from swafi.config import Config
+from swafi.impact import Impact
+from swafi.events import load_events_from_pickle
+
+
+LABEL_EVENT_FILE = 'original_w_prior_pluvial_damage_ratio'
+
+config = Config()
+
+
+def main():
+    # Load events
+    events_filename = f'events_with_target_values_{LABEL_EVENT_FILE}.pickle'
+    events = load_events_from_pickle(filename=events_filename)
+
+    # Create the impact function
+    print("\nBenchmark model (always false):")
+    bench = Impact(events, target_type='damage_ratio', random_state=42)
+    bench.create_benchmark_model('always_false')
+    bench.split_sample()
+    bench.assess_model_on_all_periods()
+
+    print("\nBenchmark model (always true):")
+    bench = Impact(events, target_type='damage_ratio', random_state=42)
+    bench.create_benchmark_model('always_true')
+    bench.split_sample()
+    bench.assess_model_on_all_periods()
+
+    print("\nBenchmark model (random):")
+    bench = Impact(events, target_type='damage_ratio', random_state=42)
+    bench.create_benchmark_model('random')
+    bench.split_sample()
+    bench.assess_model_on_all_periods()
+
+
+if __name__ == '__main__':
+    main()
