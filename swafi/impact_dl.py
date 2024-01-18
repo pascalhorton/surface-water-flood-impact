c9f1ae90a4d3435fbf91fe7a70d296a5c60145c1
Pascal Horton <pascal.horton@unibe.ch>
2024-01-18 09:40:28 +0100

    Separating the deep learning model infrastructure from its use.

--- swafi/impact_dl.py
+++ swafi/impact_dl.py
@@ -3,6 +3,7 @@ Class to compute the impact function.
 """
 
 from .impact import Impact
+from .model_dl import DeepImpact
 from .utils.data_generator import DataGenerator
 from .utils.verification import compute_confusion_matrix, print_classic_scores, \
     assess_roc_auc, compute_score_binary
@@ -10,170 +11,11 @@ from .utils.verification import compute_confusion_matrix, print_classic_scores, 
 import hashlib
 import pickle
 import keras
-from keras import layers, models
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import datetime
-
-
-class DeepImpact(models.Model):
-    """
-    Model factory.
-
-    Parameters
-    ----------
-    task: str
-        The task. Options are: 'regression', 'classification'
-    input_2d_size: list
-        The input 2D size.
-    input_1d_size: list
-        The input 1D size.
-    dropout_rate: float
-        The dropout rate.
-    with_spatial_dropout: bool
-        Whether to use spatial dropout or not.
-    with_batchnorm: bool
-        Whether to use batch normalization or not.
-    inner_activation: str
-        The inner activation function.
-    """
-
-    def __init__(self, task, input_2d_size, input_1d_size, dropout_rate=0.2,
-                 with_spatial_dropout=True, with_batchnorm=True,
-                 inner_activation='relu'):
-        super(DeepImpact, self).__init__()
-        self.model = None
-        self.task = task
-        self.input_2d_size = list(input_2d_size)
-        self.input_1d_size = list(input_1d_size)
-        self.dropout_rate = dropout_rate
-        self.nb_conv_blocks = 3
-        self.nb_filters = 64
-        self.with_spatial_dropout = with_spatial_dropout
-        self.with_batchnorm = with_batchnorm
-        self.inner_activation = inner_activation
-        self.last_activation = 'relu' if task == 'regression' else 'sigmoid'
-
-        self._check_input_size()
-        self._build_model()
-
-    def _check_input_size(self):
-        """
-        Check the input size.
-        """
-        assert len(self.input_2d_size) == 3, "Input 2D size must be 3D (with channels)"
-        assert len(self.input_1d_size) == 1, "Input 1D size must be 1D"
-
-        # Assert that the input 2D size is divisible by 2 * nb_conv_blocks
-        assert self.input_2d_size[0] % (2 * self.nb_conv_blocks) == 0, \
-            "Input 2D size must be divisible by 2 * nb_conv_blocks"
-        assert self.input_2d_size[1] % (2 * self.nb_conv_blocks) == 0, \
-            "Input 2D size must be divisible by 2 * nb_conv_blocks"
-
-    def _build_model(self):
-        """
-        Build the model.
-        """
-        input_2d = layers.Input(shape=self.input_2d_size, name='input_2d')
-        input_1d = layers.Input(shape=self.input_1d_size, name='input_1d')
-
-        # 2D convolution
-        x = input_2d
-        for i in range(self.nb_conv_blocks):
-            nb_filters = self.nb_filters * (2 ** i)
-            x = self.conv2d_block(x, filters=nb_filters, kernel_size=3)
-
-        # Flatten
-        x = layers.Flatten()(x)
-
-        # Concatenate with 1D input
-        x = layers.concatenate([x, input_1d])
-
-        # Fully connected
-        x = layers.Dense(256, activation=self.inner_activation)(x)
-        x = layers.Dense(128, activation=self.inner_activation)(x)
-        x = layers.Dense(64, activation=self.inner_activation)(x)
-        x = layers.Dense(32, activation=self.inner_activation)(x)
-
-        # Last activation
-        output = layers.Dense(1, activation=self.last_activation)(x)
-
-        self.model = models.Model(inputs=[input_2d, input_1d], outputs=output)
-
-    def conv2d_block(self, x, filters, kernel_size=3, initializer='he_normal',
-                     activation='default'):
-        """
-        Convolution block.
-
-        Parameters
-        ----------
-        x: layers.Layer
-            The input layer.
-        filters: int
-            The number of filters.
-        kernel_size: int
-            The kernel size.
-        initializer: str
-            The initializer.
-        activation: str
-            The activation function.
-
-        Returns
-        -------
-        The output layer.
-        """
-        if activation == 'default':
-            activation = self.inner_activation
-
-        x = layers.Conv2D(
-            filters=filters,
-            kernel_size=(kernel_size, kernel_size),
-            padding='same',
-            activation=activation,
-            kernel_initializer=initializer,
-        )(x)
-        x = layers.Conv2D(
-            filters=filters,
-            kernel_size=(kernel_size, kernel_size),
-            padding='same',
-            activation=activation,
-        )(x)
-
-        if self.with_batchnorm:
-            x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(
-            pool_size=(2, 2),
-        )(x)
-
-        if self.with_spatial_dropout and self.dropout_rate > 0:
-            x = layers.SpatialDropout2D(
-                rate=self.dropout_rate,
-            )(x)
-        elif self.dropout_rate > 0:
-            x = layers.Dropout(
-                rate=self.dropout_rate,
-            )(x)
-
-        return x
-
-    def call(self, inputs, **kwargs):
-        """
-        Call the model.
-
-        Parameters
-        ----------
-        inputs: list
-            The inputs.
-        **kwargs
-
-        Returns
-        -------
-        The output.
-        """
-        return self.model(inputs, **kwargs)
 
 
 class ImpactDeepLearning(Impact):
