bf420f00ac8ef07ec51758c7228d48d62eec96cc
Pascal Horton <pascal.horton@unibe.ch>
2024-09-30 15:12:42 +0200

    Working on the Transformer and adding positional encoding.

--- swafi/impact_tx_model.py
+++ swafi/impact_tx_model.py
@@ -4,6 +4,7 @@ Class for the Transformer model.
 
 from keras import layers, models
 import tensorflow as tf
+import numpy as np
 
 
 class ModelTransformer(models.Model):
@@ -54,31 +55,54 @@ class ModelTransformer(models.Model):
                 name='input_attributes')
 
             # Transformer for daily precipitation
-            x_daily = input_daily
+            x_daily_pos = self.get_positional_encoding(self.input_daily_prec_size)
+            x_daily = input_daily + x_daily_pos
+            # Project the input into the model dimension
+            x_daily = layers.Dense(
+                self.options.tx_model_dim_daily,
+                name='dense_tx_proj_daily'
+            )(x_daily)
             for _ in range(self.options.nb_transformer_blocks_daily):
                 x_daily = self.transformer_block(
                     x_daily,
+                    model_dim=self.options.tx_model_dim_daily,
                     num_heads=self.options.num_heads_daily,
                     ff_dim=self.options.ff_dim_daily,
-                    dropout_rate=self.options.dropout_rate_daily)
+                    dropout_rate=self.options.dropout_rate_daily,
+                    use_cnn=self.options.use_cnn_in_tx)
 
             # Transformer for high-frequency precipitation
-            x_high_freq = input_high_freq
+            x_high_freq_pos = self.get_positional_encoding(self.input_high_freq_prec_size)
+            x_high_freq = input_high_freq + x_high_freq_pos
+            # Project the input into the model dimension
+            x_high_freq = layers.Dense(
+                self.options.tx_model_dim_high_freq,
+                name='dense_tx_proj_high_freq'
+            )(x_high_freq)
             for _ in range(self.options.nb_transformer_blocks_high_freq):
                 x_high_freq = self.transformer_block(
                     x_high_freq,
+                    model_dim=self.options.tx_model_dim_high_freq,
                     num_heads=self.options.num_heads_high_freq,
                     ff_dim=self.options.ff_dim_high_freq,
-                    dropout_rate=self.options.dropout_rate_high_freq)
+                    dropout_rate=self.options.dropout_rate_high_freq,
+                    use_cnn=self.options.use_cnn_in_tx)
 
             # Transformer for attributes
             x_attributes = input_attributes
+            # Project the input into the model dimension
+            x_attributes = layers.Dense(
+                self.options.tx_model_dim_attributes,
+                name='dense_tx_proj_attributes'
+            )(x_attributes)
             for _ in range(self.options.nb_transformer_blocks_attributes):
                 x_attributes = self.transformer_block(
                     x_attributes,
+                    model_dim=self.options.tx_model_dim_attributes,
                     num_heads=self.options.num_heads_attributes,
                     ff_dim=self.options.ff_dim_attributes,
-                    dropout_rate=self.options.dropout_rate_attributes)
+                    dropout_rate=self.options.dropout_rate_attributes,
+                    use_cnn=False)
 
             # Concatenate
             x = layers.Concatenate(axis=-1)([x_daily, x_high_freq, x_attributes])
@@ -97,7 +121,21 @@ class ModelTransformer(models.Model):
                 shape=(self.input_attributes_size,),
                 name='input_attributes')
 
-            x = layers.Concatenate(axis=1)([input_daily, input_high_freq])
+            x_daily_pos = self.get_positional_encoding(self.input_daily_prec_size)
+            x_daily = input_daily + x_daily_pos
+            x_daily = layers.Dense(
+                self.options.tx_model_dim_daily,
+                name='dense_tx_proj_daily'
+            )(x_daily)
+
+            x_high_freq_pos = self.get_positional_encoding(self.input_high_freq_prec_size)
+            x_high_freq = input_high_freq + x_high_freq_pos
+            x_high_freq = layers.Dense(
+                self.options.tx_model_dim_high_freq,
+                name='dense_tx_proj_high_freq'
+            )(x_high_freq)
+
+            x = layers.Concatenate(axis=1)([x_daily, x_high_freq])
             # Broadcast static attributes across timesteps
             x_attributes = tf.expand_dims(
                 input_attributes,
@@ -112,9 +150,11 @@ class ModelTransformer(models.Model):
             for _ in range(self.options.nb_transformer_blocks_combined):
                 x = self.transformer_block(
                     x,
+                    model_dim=self.options.tx_model_dim_combined,
                     num_heads=self.options.num_heads_combined,
                     ff_dim=self.options.ff_dim_combined,
-                    dropout_rate=self.options.dropout_rate_combined)
+                    dropout_rate=self.options.dropout_rate_combined,
+                    use_cnn=self.options.use_cnn_in_tx)
 
         # Fully connected
         for i in range(self.options.nb_dense_layers):
@@ -123,7 +163,7 @@ class ModelTransformer(models.Model):
             else:
                 nb_units = self.options.nb_dense_units
             x = layers.Dense(nb_units, activation=self.options.inner_activation_dense,
-                             name=f'dense_{i}')(x)
+                             name=f'dense_ffn_{i}')(x)
 
             if self.options.with_batchnorm_dense:
                 x = layers.BatchNormalization(name=f'batchnorm_dense_{i}')(x)
@@ -142,42 +182,85 @@ class ModelTransformer(models.Model):
             outputs=output)
 
     @staticmethod
-    def transformer_block(x_in, num_heads=4, ff_dim=128, dropout_rate=0.1):
+    def get_positional_encoding(seq_len):
+        """
+        Get the positional encoding.
+
+        Parameters
+        ----------
+        seq_len: int
+            The sequence length.
+
+        Returns
+        -------
+        The positional encoding.
+        """
+        d_model = 1
+
+        pos = np.arange(seq_len)[:, np.newaxis]
+        i = np.arange(d_model)[np.newaxis, :]
+        angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+
+        # Apply sin to even indices and cos to odd indices
+        pos_encoding = np.zeros((seq_len, d_model))
+        pos_encoding[:, 0::2] = np.sin(pos * angle_rates[:, 0::2])  # Even
+        pos_encoding[:, 1::2] = np.cos(pos * angle_rates[:, 1::2])  # Odd
+
+        # Squeeze as d_model is 1
+        pos_encoding = pos_encoding.squeeze()
+
+        pos_encoding = pos_encoding[np.newaxis, ...]
+
+        return tf.cast(pos_encoding, dtype=tf.float32)
+
+    def transformer_block(self, inputs, model_dim=512, num_heads=8, ff_dim=128,
+                          dropout_rate=0.1, use_cnn=False):
         """
         Transformer encoder.
 
         Parameters
         ----------
-        x_in: tensor
+        inputs: tensor
             The input tensor.
         num_heads: int
             The number of heads.
+        model_dim: int
+            The model dimension.
         ff_dim: int
             The feed-forward dimension.
         dropout_rate: float
             The dropout rate.
+        use_cnn: bool
+            Whether to use a CNN or not. If not, a dense layer is used.
 
         Returns
         -------
         The output tensor.
         """
+        key_dim = model_dim // num_heads
+
         # Self-attention
-        x_attn = layers.MultiHeadAttention(num_heads,x_in.shape[-1])(x_in, x_in)
-        x_attn = layers.Dropout(dropout_rate)(x_attn)
-        x_attn = layers.LayerNormalization()(x_in + x_attn)
+        x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+        x = layers.MultiHeadAttention(num_heads=num_heads, key_dim=key_dim)(x, x)
+        x = layers.Dropout(dropout_rate)(x)
+        res = layers.Add()([inputs, x])
 
         # Feed-forward network
-        layer_name = f'dense_tx_{int(1e4 * tf.random.uniform([]))}'
-        x_ffn = layers.Dense(ff_dim, activation='relu', name=layer_name
-                             )(x_attn)
-        layer_name = f'dense_tx_{int(1e4 * tf.random.uniform([]))}'
-        x_ffn = layers.Dense(x_in.shape[-1], name=layer_name
-                             )(x_ffn)
-        x_ffn = layers.Dropout(dropout_rate)(x_ffn)
-        x_ffn = layers.Add()([x_attn, x_ffn])
-        x_ffn = layers.LayerNormalization()(x_ffn)
+        x = layers.LayerNormalization(epsilon=1e-6)(res)
+        if use_cnn:
+            x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation='relu')(x)
+            x = layers.Dropout(dropout_rate)(x)
+            x = layers.Conv1D(filters=model_dim, kernel_size=1)(x)
+        else:
+            layer_name = f'dense_tx_{int(1e4 * tf.random.uniform([]))}'
+            x = layers.Dense(ff_dim, activation='relu', name=layer_name)(x)
+            layer_name = f'dense_tx_{int(1e4 * tf.random.uniform([]))}'
+            x = layers.Dense(model_dim, name=layer_name)(x)
+            x = layers.Dropout(dropout_rate)(x)
 
-        return x_ffn
+        x = layers.Add()([x, res])
+
+        return x
 
     def call(self, inputs, **kwargs):
         """
