3e74f13e7638ea82278f89f7750047a670f37222
Pascal Horton <pascal.horton@unibe.ch>
2024-12-11 18:23:49 +0100

    Adding weight denominator assessment to LR models.

--- scripts/impact_functions/train_lr_occurrence.py
+++ scripts/impact_functions/train_lr_occurrence.py
@@ -10,6 +10,14 @@ from swafi.impact_lr import ImpactLogisticRegression
 
 config = Config()
 
+# Define the weight denominators. Optimal value is 30 for GVZ and 45 for Mobiliar.
+weight_denominators = [30]
+
+# Enable to test different weight denominators
+# weight_denominators = [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 80, 100]
+
+random_states = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 42]
+
 
 def main():
     options = ImpactBasicOptions()
@@ -20,21 +28,27 @@ def main():
     events_filename = f'events_{options.dataset}_with_target_{options.event_file_label}.pickle'
     events = load_events_from_pickle(filename=events_filename)
 
-    # Create the impact function
-    lr = ImpactLogisticRegression(events, options)
+    for random_state in random_states:
+        options.random_state = random_state
+        options.print_options()
 
-    lr.select_features(options.replace_simple_features)
-    lr.load_features(options.simple_feature_classes)
+        for weight_denominator in weight_denominators:
+            # Create the impact function
+            lr = ImpactLogisticRegression(events, options)
 
-    lr.split_sample()
-    lr.normalize_features()
-    lr.compute_balanced_class_weights()
-    lr.compute_corrected_class_weights(weight_denominator=10)
-    lr.fit()
+            lr.select_features(options.replace_simple_features)
+            lr.load_features(options.simple_feature_classes)
 
-    tag_atts = options.get_attributes_tag()
+            lr.split_sample()
+            lr.normalize_features()
+            lr.compute_balanced_class_weights()
+            lr.compute_corrected_class_weights(weight_denominator=weight_denominator)
+            lr.fit()
 
-    lr.assess_model_on_all_periods(save_results=True, file_tag=f'lr_{tag_atts}')
+            tag_atts = options.get_attributes_tag()
+
+            file_tag = f'lr_{tag_atts}_wd_{weight_denominator}'
+            lr.assess_model_on_all_periods(save_results=True, file_tag=file_tag)
 
 
 if __name__ == '__main__':
