9fc49695c92711ddee40988a2d72af5db0621215
Pascal Horton <pascal.horton@unibe.ch>
2025-08-20 22:43:53 +0200

    Bumping version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="swafi",
-    version="0.1.0",
+    version="0.2.0",
     author="Pascal Horton",
     author_email="pascal.horton@unibe.ch",
     description="Surface Water Flood Impact",
