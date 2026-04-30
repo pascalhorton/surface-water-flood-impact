3e735263b8a07fca18c272c2b0f6575bd25a5fd6
Pascal Horton <pascal.horton@unibe.ch>
2026-04-30 10:44:45 +0200

    Stratify by default.

--- swafi/impact.py
+++ swafi/impact.py
@@ -268,7 +268,7 @@ class Impact:
         self.df = self.df[(self.df['nb_claims'] == 0) |
                           (self.df['nb_claims'] >= threshold)]
 
-    def split_sample(self, valid_test_size=0.25, test_size=0, ref_date='i_max_only', stratify=False, stratify_by='month'):
+    def split_sample(self, valid_test_size=0.3, test_size=0, ref_date='i_max_only', stratify=True, stratify_by='day'):
         """
         Split the sample into training, validation and test sets. The split is
         stratified on the target, i.e. the proportion of events with and without
